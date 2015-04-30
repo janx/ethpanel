5bf0db8b3eea67de5da5b52c364c1f05229fae8a
Jan Xie <jan.h.xie@gmail.com>
2015-04-30 17:02:52 +0800

    refactor EthService

--- js/services/EthService.js
+++ js/services/EthService.js
@@ -8,38 +8,53 @@ function EthService(url, options) {
 
   this.web3 = require('web3');
   this.web3.setProvider(new this.web3.providers.HttpProvider(url));
+
+  this.watchers = {};
 }
 
-EthService.prototype.getNumber = function() {
-  return this.web3.eth.blockNumber;
-};
+assign(EthService.prototype, {
+  getNumber: function() {
+    return this.web3.eth.blockNumber;
+  },
 
-EthService.prototype.getBlocks = function() {
-  return this.blocks;
-};
+  getBlocks: function() {
+    return this.blocks;
+  },
 
-EthService.prototype.start = function() {
-  var number = this.getNumber();
+  getNetStats: function() {
+    return {
+      listening: this.web3.net.listening,
+      peerCount: this.web3.net.peerCount
+    };
+  },
 
-  this.blocks = [];
-  for(var i=0; i < this.options.blocksLimit; i++) {
-    this.blocks.push( this.web3.eth.getBlock(number-i, false) );
-  }
+  start: function() {
+    var number = this.getNumber();
 
-  MyEthActions.setupBlocks(this.blocks);
+    this.blocks = [];
+    for(var i=0; i < this.options.blocksLimit; i++) {
+      this.blocks.push( this.web3.eth.getBlock(number-i, false) );
+    }
 
-  var callback = function() {
+    MyEthActions.setupBlocks(this.blocks);
+    //MyEthActions.setupNetStats(this.getNetStats());
+
+    this.watchers['blocks'] = window.setInterval(this._onNewBlock.bind(this), 2000);
+  },
+
+  stop: function() {
+    for(var name in this.watchers) {
+      window.clearInterval(this.watchers[name]);
+    }
+  },
+
+  _onNewBlock: function() {
     var number = this.getNumber();
     for(var i=this.blocks[0].number+1; i <= number; i++) {
       this.blocks.unshift(this.web3.eth.getBlock(i, false))
       MyEthActions.newBlock(this.blocks[0]);
     }
-  };
-  this.watcher = window.setInterval(callback.bind(this), 2000);
-};
-
-EthService.prototype.stop = function() {
-  window.clearInterval(this.watcher);
-};
+  }
+});
 
 module.exports = EthService;

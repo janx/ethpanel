b852b4676142afc938a962462340a1d7f122bdd1
Jan Xie <jan.h.xie@gmail.com>
2015-05-12 10:09:26 +0800

    src scaffold

---
+++ src/app/app.jsx
@@ -0,0 +1,32 @@
+(function () {
+  var React = require('react');
+  //var EthPanel = require('./components/EthPanel.react');
+  //var EthPanelError = require('./components/EthPanelError.react');
+  //var EthService = require('./services/EthService');
+  var injectTapEventPlugin = require("react-tap-event-plugin");
+
+  //Needed for React Developer Tools
+  window.React = React;
+
+  //Needed for onTouchTap
+  //Can go away when react 1.0 release
+  //Check this repo:
+  //https://github.com/zilverline/react-tap-event-plugin
+  injectTapEventPlugin();
+
+  try {
+    var service = new EthService('http://localhost:8545');
+    service.start();
+
+    React.render(
+      <h1>Hello world!</h1>,
+      document.body
+    );
+  } catch(error) {
+    React.render(
+      <h1>Error!</h1>,
+      document.body
+    );
+  }
+})();
+

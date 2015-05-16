8a8284d5608adfbb8e78d906720909b015fea170
Jan Xie <jan.h.xie@gmail.com>
2015-05-16 13:53:34 +0800

    remove tap event plugin

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,16 +3,9 @@
   var EthPanel = require('./components/EthPanel');
   var EthPanelError = require('./components/EthPanelError');
   var EthWebAPIUtils = require('./utils/EthWebAPIUtils');
-  var injectTapEventPlugin = require("react-tap-event-plugin");
 
   //Needed for React Developer Tools
   window.React = React;
-
-  //Needed for onTouchTap
-  //Can go away when react 1.0 release
-  //Check this repo:
-  //https://github.com/zilverline/react-tap-event-plugin
-  injectTapEventPlugin();
 
   try {
     React.render(

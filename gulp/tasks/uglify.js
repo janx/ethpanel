4892228da3aa6fba84b6f0a891e10c23bc612298
Jan Xie <jan.h.xie@gmail.com>
2015-05-20 15:57:24 +0800

    remove gulp

--- gulp/tasks/uglify.js
+++
@@ -1,9 +0,0 @@
-var gulp   = require('gulp');
-var uglify = require('gulp-uglify');
-var config = require('../config').uglify;
-
-gulp.task('uglify', ['browserify'], function(callback) {
-  return gulp.src(config.src)
-    .pipe(uglify())
-    .pipe(gulp.dest(config.dest));
-});

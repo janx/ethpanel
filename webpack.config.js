bc9610a557223eadd29cc4667b27f6d3006906da
Jan Xie <jan.h.xie@gmail.com>
2015-05-20 15:46:36 +0800

    polish build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
   entry: {
     app: [ "./app/js/app.jsx" ]
   },
   output: {
-    path: path.join(__dirname, 'build', 'js'),
-    filename: "[name].js"
+    path: path.join(__dirname, 'build'),
+    filename: "js/[name].js"
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/,                   loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
+      { test: /\.jsx?$/,                   loader: 'jsx?harmony', exclude: /node_modules/ },
       { test: /\.json$/,                   loader: 'json' },
 
-      { test: /\.css$/,                    loader: 'style!css' },
-      { test: /\.less$/,                   loader: 'style!css!less' },
+      { test: /\.css$/,                    loader: ExtractTextPlugin.extract('style', 'css') },
+      { test: /\.less$/,                   loader: ExtractTextPlugin.extract('style', 'css!less') },
 
-      { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader?limit=10000' },
-      { test: /\.(woff|woff2)$/,           loader: 'url-loader?limit=100000' },
-      { test: /\.(ttf|eot)$/,              loader: 'file-loader' }
+      { test: /\.(png|jpg|jpeg|gif)$/,     loader: 'url-loader?limit=10000&name=images/[name].[ext]' },
+      { test: /\.(woff|woff2)$/,           loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+      { test: /\.(ttf|eot|svg)$/,          loader: 'file-loader?name=fonts/[name].[ext]' }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   plugins: [
+    new ExtractTextPlugin("css/[name].css", {
+      allChunks: true
+    }),
     new HtmlWebpackPlugin({
       template: './app/index.html',
       inject: true
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,9 @@ config.entry.app = [
   "webpack-dev-server/client?http://localhost:8080",
   "webpack/hot/only-dev-server",
 ].concat(config.entry.app);
-console.log(config.entry);
+
+delete config.module.loaders[0].loader
+config.module.loaders[0].loaders = ['react-hot', 'jsx?harmony'];
 
 config.plugins.push( new webpack.HotModuleReplacementPlugin() );
 config.plugins.push( new webpack.NoErrorsPlugin() );
